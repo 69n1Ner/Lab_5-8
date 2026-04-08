0b7565c671d6930cc0ac05c991efe3fd553658ae
69n1Ner_ <69n1Ner_@niuitmo.ru>
2026-04-08 11:39:24 +0300

    исправлене Cont_Wrapper

--- Lab_5_mvn/src/main/java/IO/ContainerWrapper.java
+++ Lab_5_mvn/src/main/java/IO/ContainerWrapper.java
@@ -25,7 +25,4 @@ public class ContainerWrapper {
         return organizations;
     }
 
-    public void setOrganizations(List<Organization> organizations) {
-        this.organizations = organizations;
-    }
 }
